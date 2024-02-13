a2919b408d153903b29f0e638f2cc1638088e115
심상갑 <kappa2410@gmail.com>
2024-02-13 14:15:11 +0900

    Initial Commit

---
+++ SwiftUIToolTipBox/SwiftUIToolTipBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToolTipBoxApp.swift
+//  SwiftUIToolTipBox
+//
+//  Created by Gab on 2024/02/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToolTipBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
