27e064781e538ccf10f8b929ba6c7134e0d43d71
심상갑 <kappa2410@gmail.com>
2024-02-14 10:08:57 +0900

    Fix: 패키지 수정

--- Package.swift
+++ Package.swift
@@ -5,17 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUIToolTipBox",
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "SwiftUIToolTipBox",
+            name: "ToolTipBox",
             targets: ["SwiftUIToolTipBox"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftUIToolTipBox"),
+            name: "SwiftUIToolTipBox",
+            dependencies: []),
         .testTarget(
             name: "SwiftUIToolTipBoxTests",
             dependencies: ["SwiftUIToolTipBox"]),
