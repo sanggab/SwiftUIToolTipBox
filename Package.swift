8ad452e6703fc8256fc330b687709bc1a55ba93b
심상갑 <kappa2410@gmail.com>
2024-02-14 10:49:45 +0900

    Feat: Path 추가

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftUIToolTipBox",
-            dependencies: []),
+            path: "Sources"),
         .testTarget(
             name: "SwiftUIToolTipBoxTests",
             dependencies: ["SwiftUIToolTipBox"]),
